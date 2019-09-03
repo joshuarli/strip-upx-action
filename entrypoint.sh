ccf366431127f98ea68b2e599f664a056184a6b5
Joshua Li <josh@jrl.ninja>
2019-09-03 01:45:43 -0700

    templatize entrypoint with jobs.<job_id>.steps.with keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 # i'm guessing this is the volume-mounted directory where steps: executes in
 cd "$GITHUB_WORKSPACE"
 
-# TODO: see TODO in .github/workflows/test.yml
 echo "running strip"
-strip -v --strip-all ./hi
+strip "$INPUT_STRIP_ARGS" "$INPUT_INFILE"
 
 echo "running upx"
-upx -9 ./hi
+upx "$INPUT_UPX_ARGS" "$INPUT_INFILE"
