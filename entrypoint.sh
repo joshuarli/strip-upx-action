a8ad400a7deb15c07c9d649a2c2ded6512295ff5
Joshua Li <josh@jrl.ninja>
2019-09-03 02:01:23 -0700

    print size changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,16 @@
 # i'm guessing this is the volume-mounted directory where steps: executes in
 cd "$GITHUB_WORKSPACE"
 
+report_size () {
+    printf '\nsize: %s\n\n' "$(ls -lh ${INPUT_INFILE} | awk '{print $5}')"
+}
+
+report_size
 echo "running strip"
 strip "$INPUT_STRIP_ARGS" "$INPUT_INFILE"
 
+report_size
 echo "running upx"
 upx "$INPUT_UPX_ARGS" "$INPUT_INFILE"
+
+report_size
